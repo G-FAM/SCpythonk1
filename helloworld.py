2dcb3f07d3b5ac3cfd9d79cce55fed6c33bf3855
Famuagun Gbenga Collins <gbengafamuagun@gmail.com>
2018-07-24 14:30:24 +0000

    first program hello world

---
+++ helloworld.py
@@ -0,0 +1,18 @@
+print("Hello World")
+print("It's a beautiful day in the neighbor hood")
+
+print 2+4
+print 2+(4*7)
+print 2**2
+print 10%3
+
+x = 3
+y = 2
+if x==y:
+    print ("Yay")
+else:
+    print ("Nay")
+    
+    name = ("Gbenga")
+print name
+print(2+4)
